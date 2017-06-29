5b6d92f6f2992d198ee2df8debf35cb6a7bcc635
jw <joonwoo3816@gmail.com>
2017-06-29 16:56:06 +0900

    4L2

---
+++ Book/12.LoopTypes_P57.cpp
@@ -0,0 +1,16 @@
+//do¡¦while Loop
+#include <iostream>
+using namespace std;
+int main ()
+{
+// Local variable declaration:
+int a = 10;
+// do loop execution
+do
+{
+cout << "value of a: " << a << endl;
+a = a + 1;
+}while( a < 20 );
+return 0;
+}
+
