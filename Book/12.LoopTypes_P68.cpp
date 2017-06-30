12955622385270bac39a6f4701339c8d0e04c3b4
jw <joonwoo3816@gmail.com>
2017-06-30 10:52:52 +0900

    5H

---
+++ Book/12.LoopTypes_P68.cpp
@@ -0,0 +1,12 @@
+// The Infinite Loop
+//NOTE: You can terminate an infinite loop by pressing Ctrl + C keys.
+#include <iostream>
+using namespace std;
+int main ()
+{
+for( ; ; )
+{
+printf("This loop will run forever.\n");
+}
+return 0;
+}
