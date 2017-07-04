5b88bf6cec81ccf07a73599b83037e9d5c48eddd
jw <joonwoo3816@gmail.com>
2017-07-04 10:58:16 +0900

    9L2

--- CppBook/15.Numbers_p93.cpp
+++ CppBook/15.Numbers_p93.cpp
@@ -1,34 +1,32 @@
-//Default Values for Parameters
-//function definition to swap the value.
+//Defining Numbers
 
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b=20)
-{
-    int result;
-
-    result = a + b;
-
-    return (result);
-
-}
-
 int main()
 {
     //local vatiable declaration:
-    int a=100;
-    int b=200;
-    int result;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+
+
+    // number assignments;
+    s=10;
+    i=1000;
+    l=1000000;
+    f=230.47;
+    d=30949.374;
     
-    // calling a function to add the values.
-    result = sum(a,b);
-    cout << "Total value is :" << result << endl;
-
-    // calling a function again as follows.
-    result = sum(a);
-    cout << "Total value is :" << result << endl;
-
+    // number printing;
+    cout << "short  s:" << s << endl;
+    cout << "int    i:" << i << endl;
+    cout << "long   l:" << l << endl;
+    cout << "float  f:" << f << endl;
+    cout << "double d:" << d << endl;
+    
     return 0;
 }
 
