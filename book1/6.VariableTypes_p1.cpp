f4852f4143c156cca0e178573c0a99f430a079da
nomadlife15u <aaeacd@gmail.com>
2019-01-28 21:09:41 +0900

    cpp example code updated

--- book1/6.VariableTypes_p1.cpp
+++ book1/6.VariableTypes_p1.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
+
 // Variable declaration:
 extern int a, b;
 extern int c;
 extern float f;
-int main ()
+
+int main()
 {
-// Variable definition:
-int a, b;
-int c;
-float f;
-// actual initialization
-a = 10;
-b = 20;
-c = a + b;
-cout << c << endl ;
-f = 70.0/3.0;
-cout << f << endl ;
-return 0;
+    int a, b;
+    int c;
+    float f;
+
+    a = 10;
+    b = 20;
+    c = a + b;
+    cout << c << endl;
+    f = 70.0 / 3.0;
+    cout << f << endl;
+    return 0;
 }
